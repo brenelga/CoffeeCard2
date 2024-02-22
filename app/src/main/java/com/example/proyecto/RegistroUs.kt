cdc1ad13ca17e9a9819ffe7ae2db4e6600cba6c0
Brenel Galicia <jesus.galicia.23e@utzmg.edu.mx>
2024-02-22 08:15:49 -0600

    Actualización 22 de febrero del 2024

--- app/src/main/java/com/example/proyecto/RegistroUs.kt
+++ app/src/main/java/com/example/proyecto/RegistroUs.kt
@@ -1,4 +1,11 @@
 package com.example.proyecto
 
-class RegistroUs {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroUs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registro_usuario)
+    }
 }
